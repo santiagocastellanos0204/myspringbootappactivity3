9ff544f73278e22d8792557b6d0208cf9f749b49
Santiago Castellanos <sjcastellanos0204@gmail.com>
2025-04-02 20:46:09 -0700

    Update OrdersBusinessService.java

--- src/main/java/com/gcu/business/OrdersBusinessService.java
+++ src/main/java/com/gcu/business/OrdersBusinessService.java
@@ -2,30 +2,36 @@ package com.gcu.business;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.gcu.data.DataAccessInterface;
 import com.gcu.model.OrderModel;
 
-public class OrdersBusinessService implements OrdersBusinessInterface{
-	
-	//Declaring and Initializing
-	private DataAccessInterface<OrderModel> service;
+public class OrdersBusinessService implements OrdersBusinessInterface {
 
-	/**
-	 * Setter for Service DataAccessInterface<OrderModel>
-	 * @param service Specified for Service
-	 */
+    private static final Logger logger = LoggerFactory.getLogger(OrdersBusinessService.class);
+
+    // Declaring and Initializing
+    private DataAccessInterface<OrderModel> service;
+
+    /**
+     * Setter for Service DataAccessInterface<OrderModel>
+     * @param service Specified for Service
+     */
     @Autowired
     public void setService(DataAccessInterface<OrderModel> service) {
         this.service = service;
+        logger.info("DataAccessInterface service has been set.");
     }
-    
-	/**
+
+    /**
      * A test method to print a message.
      */
     @Override
     public void test() {
+        logger.info("Executing test method in OrdersBusinessService.");
         System.out.println("Hello from the OrdersBusinessService");
     }
 
@@ -36,26 +42,34 @@ public class OrdersBusinessService implements OrdersBusinessInterface{
      */
     @Override
     public List<OrderModel> getOrders() {
-        // Hard-coded list of orders
-        return service.findAll();
+        logger.info("Fetching list of orders...");
+        try {
+            List<OrderModel> orders = service.findAll();
+            logger.debug("Retrieved {} orders from the database.", orders.size());
+            return orders;
+        } catch (Exception e) {
+            logger.error("Error retrieving orders: {}", e.getMessage(), e);
+            return List.of(); // Return an empty list on failure
+        }
     }
-    
+
     /**
      * Initialization method.
      * This method is invoked by the container after instantiation but before dependency injection.
      */
     @Override
     public void init() {
+        logger.info("OrdersBusinessService initialized.");
         System.out.println("Hello from the OrdersBusinessService from the init() method");
     }
-    
+
     /**
      * Destruction method.
      * This method is invoked by the container before the bean is destroyed.
      */
     @Override
     public void destroy() {
+        logger.info("OrdersBusinessService is being destroyed.");
         System.out.println("Hello from the OrdersBusinessService from the destroy() method");
     }
-    
 }
